cd826e0bfec9e28c958422e637a97d014f92195e
Spare <Spare@Heroku.com>
2023-11-26 15:43:15 +0700

    adding about me and app.css and app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
 import "./App.css";
-//import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+import Contact from "./components/contact";
+import Main from "./components/main";
+import Navbar from "./components/navbar";
+import Project from "./components/project";
+import { ToastContainer, toast } from "react-toastify";
 function App() {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!isDarkMode);
+
+    if (!isDarkMode) {
+      toast.success("Dark Mode Activated", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    } else {
+      toast.success("Light Mode Activated", {
+        position: "bottom-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
   return (
     <>
-      <div className="navbar">
-        <div>
-          <a className="logo" href="">
-            Satrio
-          </a>
-        </div>
-        <div>
-          <ul className="nav-links">
-            <li className="nav-items">
-              <a href="">About</a>
-            </li>
-            <li className="nav-items">
-              <a href="">Skills</a>
-            </li>
-            <li className="nav-items">
-              <a href="">Contact</a>
-            </li>
-          </ul>
-        </div>
+      <div className={`app ${isDarkMode ? "dark-mode" : ""}`}>
+        <Navbar toggleDarkMode={toggleDarkMode}></Navbar>
+        <Main></Main>
+        <Project></Project>
+        <Contact></Contact>
+        <ToastContainer />
       </div>
     </>
   );

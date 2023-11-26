f4a1413078379e2684872107f6899970875f8062
Spare <Spare@Heroku.com>
2023-11-26 15:44:40 +0700

    adding project to the react

---
+++ src/components/project.tsx
@@ -0,0 +1,27 @@
+import GetData from "../function/GetData";
+import "../project.css";
+function Project() {
+  const repositoryLinks: string[] = [
+    "https://api.github.com/repos/sdssaeq/CapthaSolver",
+    "https://api.github.com/repos/sdssaeq/SpareDiscordBot",
+    "https://api.github.com/repos/sdssaeq/web_app",
+    "https://api.github.com/repos/sdssaeq/portofolio",
+
+    // Add more repository links as needed
+  ];
+  return (
+    <>
+      <section id="project" className="section">
+        <div className="card-project">
+          <h1>My Github Project</h1>
+        </div>
+        <div className="card-project">
+          {repositoryLinks.map((link, index) => (
+            <GetData key={index} link={link} />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+export default Project;
